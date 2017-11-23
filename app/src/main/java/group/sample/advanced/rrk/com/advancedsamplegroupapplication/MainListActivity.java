743959eea112e1d107bd98bc68eef6677c7fc2e6
akakim <daphne5009@gmail.com>
2017-11-24 01:50:10 +0900

    TabLayout과 ViewPager 를 이용한 샘플 작성. 원하는만큼 나왔다. 다만 FragmentList를 좀 더 유연하게 쓰기위해 상속구조를 이용해야겟다.

--- app/src/main/java/group/sample/advanced/rrk/com/advancedsamplegroupapplication/MainListActivity.java
+++ app/src/main/java/group/sample/advanced/rrk/com/advancedsamplegroupapplication/MainListActivity.java
@@ -15,8 +15,9 @@ import butterknife.ButterKnife;
 import group.sample.advanced.rrk.com.advancedsamplegroupapplication.adapter.SampleListAdapter;
 import group.sample.advanced.rrk.com.advancedsamplegroupapplication.data.SampleItem;
 import group.sample.advanced.rrk.com.advancedsamplegroupapplication.samples.IdentifyActivity;
+import group.sample.advanced.rrk.com.advancedsamplegroupapplication.samples.OpenViewPagerActivity;
 
-public class MainListActivity extends BaseActivity implements SampleListAdapter.ItemClickListener{
+public class MainListActivity extends BaseActivity implements SampleListAdapter.ItemClickListener {
 
 
     @BindView(R.id.rvSampleList)
@@ -35,6 +36,7 @@ public class MainListActivity extends BaseActivity implements SampleListAdapter.
         ButterKnife.bind(this);
 
         sampleItems.add( new SampleItem(IdentifyActivity.class,"Application 고유 ID 와 device의 고유 ID"));
+        sampleItems.add( new SampleItem(OpenViewPagerActivity.class,"뷰페이저 테스트를위한 페이지"));
 
 
         sampleListAdapter = new SampleListAdapter(this, sampleItems , this  );
